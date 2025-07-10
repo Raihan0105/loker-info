736464a2da8314d38cf6f8c43396de4fe27da8d6
Raihan <canggihraihan9@gmail.com>
2025-07-10 20:14:20 +0700

    first commit

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,16 @@
+<style>
+  .footer {
+    background-color: #f2f2f2;
+    padding: 20px;
+    text-align: center;
+    border-top: 1px solid #e7e7e7;
+    margin-top: 30px;
+    font-family: sans-serif;
+  }
+</style>
+
+<footer class="footer">
+  <p>Ingin memasang informasi lowongan pekerjaan di sini?</p>
+  <p>Hubungi kami melalui:</p>
+  <p><strong>Email:</strong> emailanda@gmail.com | <strong>Telepon/WhatsApp:</strong> +62 812-3456-7890</p>
+</footer>
